9b0ab2ee2e1f4621ea941dccb9094d10286816d4
Adel inoubli <adelinoubli@gmail.com>
2023-09-13 11:53:31 +0200

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Adel on 9/13/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
